bd32982d3c5cd6b01bf88afd0bf02fae6b28b98c
Michael Sobczak <michaelsobczak54@gmail.com>
2012-11-12 05:31:34 -0500

    Updated the draw_map module
    now shows production numbers

--- settlers/settlers/graphics/draw_map.py
+++ settlers/settlers/graphics/draw_map.py
@@ -1,11 +1,28 @@
 from settlers import RESOURCE_DIR as IMG_DIR
 from settlers.game import model
 
-import Image, os
+import Image, ImageFont, ImageDraw, os
 
 tile_images = [ Image.open( os.path.join(IMG_DIR, '%s.png' % tile_type.lower()) ) for tile_type in model.TILE_TYPES ]
 
 tile_img_map = dict(zip(model.TILE_TYPES, tile_images))
+
+board_font = ImageFont.truetype(os.path.join(IMG_DIR, 'roboto', 'Roboto-Black.ttf'), 60)
+
+def draw_production(tile_image, production_number):
+    if production_number <= 0:
+        return tile_image
+    tw, th = board_font.getsize(str(production_number))
+    iw, ih = tile_image.size
+    text_pos = (iw - tw) / 2, (ih - th) / 2
+    new_image = Image.new('RGBA', (iw, ih))
+    new_image.paste(tile_image, (0,0), tile_image)
+    draw = ImageDraw.Draw(new_image)
+    draw.rectangle([text_pos, (text_pos[0] + tw, text_pos[1] + th)], fill=(255,255,207))
+    text_color = (255,0,0) if production_number == 6 or production_number == 8 else (0,0,0)
+    draw.text(text_pos, str(production_number), font=board_font, fill=text_color)
+    del draw
+    return new_image
 
 def get_map_image(tile_map):
     
@@ -18,7 +35,8 @@ def get_map_image(tile_map):
         for y_idx, tile in tile_column.iteritems():
             sx = int(tw * x_idx * (0.75))
             sy = int(th * (0.5*x_idx + y_idx))
-            tile_screen_coords.append( ((sx, sy), tile_img_map[tile.type]))
+            tile_image = draw_production(tile_img_map[tile.type], tile.production)
+            tile_screen_coords.append( ((sx, sy), tile_image))
             
     x_coords = [ s_coord[0] for s_coord, tile in tile_screen_coords ]
     y_coords = [ s_coord[1] for s_coord, tile in tile_screen_coords ]
--- settlers/settlers/simulator.py
+++ settlers/settlers/simulator.py
@@ -1,4 +1,9 @@
 
 
+class Player(object):
+    
+    def __init__(self, name):
+        pass
+    
 def simulate():
     pass
